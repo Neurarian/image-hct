6abf4ead23ddf400ab09a59c62f2eb391ff886d3
Neurarian <110474238+Neurarian@users.noreply.github.com>
2025-04-12 19:43:11 +0200

    nix: format flake

--- flake.nix
+++ flake.nix
@@ -9,38 +9,44 @@
     };
   };
 
-  outputs = { nixpkgs, flake-parts, systems, ... } @ inputs:
-    let
-      mkPkgs = system:
-        import nixpkgs {
-          inherit system;
-        };
-    in
-      flake-parts.lib.mkFlake { inherit inputs; } {
-        systems = import systems;
-        
-        perSystem = { system, config, ... }: let
-          pkgs = mkPkgs system;
-        in {
+  outputs = {
+    nixpkgs,
+    flake-parts,
+    systems,
+    ...
+  } @ inputs: let
+    mkPkgs = system:
+      import nixpkgs {
+        inherit system;
+      };
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems;
 
-          packages.default = pkgs.rustPlatform.buildRustPackage {
-            pname = "image-hct";
-            version = "0.1.0";
-            src = pkgs.lib.cleanSource ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-          };
-          
-          # Development shell
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [ config.packages.default ];
-            buildInputs = with pkgs; [
-              rust-bin.stable.latest.default
-              rust-analyzer
-              rustfmt
-              clippy
-            ];
-          };
+      perSystem = {
+        system,
+        config,
+        ...
+      }: let
+        pkgs = mkPkgs system;
+      in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "image-hct";
+          version = "0.1.0";
+          src = pkgs.lib.cleanSource ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        # Development shell
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [config.packages.default];
+          buildInputs = with pkgs; [
+            rust-bin.stable.latest.default
+            rust-analyzer
+            rustfmt
+            clippy
+          ];
         };
       };
+    };
 }
-
